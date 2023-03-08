eebd86463c3e5d51219bef788b4e6a17eb58121d
zororaka00 <rakawidhiantoro@gmail.com>
2023-03-09 01:42:32 +0700

    .

--- scripts/deploy_production.ts
+++ scripts/deploy_production.ts
@@ -14,6 +14,16 @@ async function main() {
   .deploy("0x4200000000000000000000000000000000000006", "0xe592427a0aece92de3edee1f18e0157c05861564");
   await instance_swap_sc99.deployed();
   console.log(`Address Swap Contract: ${instance_swap_sc99.address}`);
+  
+  let instance_nft_reward_sc99 = await (await ethers.getContractFactory("NFTRewardSC99"))
+  .deploy("0x7f5c764cbc14f9669b88837ca1490cca17c31607", "0xB598c2Ca6323aE388A260E215817fE49d80916a6");
+  await instance_nft_reward_sc99.deployed();
+  console.log(`Address Swap Contract: ${instance_nft_reward_sc99.address}`);
+
+  let instance_reward_sc99 = await upgrades.deployProxy((await ethers.getContractFactory("RewardSC99")),
+  ["0x7f5c764cbc14f9669b88837ca1490cca17c31607", "0xc45376BE05153316b01E06FC3e341feF51283322"], { kind: 'uups' });
+  await instance_reward_sc99.deployed();
+  console.log(`Address Swap Contract: ${instance_reward_sc99.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
